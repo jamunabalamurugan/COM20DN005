411c0cac79382650a88c00913589cecc9e807942
Jamuna Balamurugan <jamunabalamurugan@yahoo.com>
2021-01-27 18:09:52 +0530

    Day7 Demos DLL,Collections,Generic

---
+++ C#_Day7/Part1/PrivateAssembly/ClientUsingDLL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoDLL;
+namespace ClientUsingDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardValidator cv = new CardValidator() ;
+           Console.WriteLine("Enter a card no");
+            string cardno = Console.ReadLine();
+            if(cv.Validate(cardno))
+                Console.WriteLine("Valid Card number");
+            else
+                Console.WriteLine("Invalid Card number");
+
+            Console.ReadKey();
+        }
+    }
+}
