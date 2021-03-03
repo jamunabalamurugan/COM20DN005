720d69b13ecc70adddf785fd08fcb74c355029da
Jamuna Balamurugan <jamunabalamurugan@yahoo.com>
2021-03-03 19:34:25 +0530

    MVC Code First Controller,HTML Helpers,RemoteValidation,StateManagement

---
+++ ASP.Net MVC/MVCViewsEg/MVCViewsEg/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCViewsEg.Models;
+
+namespace MVCViewsEg.Controllers
+{
+    public class LoginController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(LoginDemoVM login)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = (from c in db.Customers
+                              where c.CustomerID == login.CustomerID
+                              && c.CompanyName == login.CompanyNme
+                              select c.ContactName).SingleOrDefault();
+
+                if (result != null)
+                {
+                    Session["Name"] = result;
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Invalid Login Credentials...Pls try again");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ ASP.Net MVC/MVCViewsEg/MVCViewsEg/Models/LoginDemoVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCViewsEg.Models
+{
+    public class LoginDemoVM
+    {
+        [Key]
+        [Required]
+        public string CustomerID { get; set; }
+        [DisplayName("Enter Password")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string CompanyNme { get; set; }
+    }
+}
